f1e1909fbc943c778f73d91c1c6e1aed245c4297
bartp <bartek.paraf@gmail.com>
2024-01-28 16:10:07 +0100

    colorscheme changed to catppuccin

--- lua/bp/plugins/colorscheme.lua
+++ lua/bp/plugins/colorscheme.lua
@@ -1,8 +1,21 @@
+-- return {
+--     "sainnhe/edge",
+--     priority = 1000, -- load this before anything else
+--     config = function()
+--         vim.cmd([[let g:edge_style='aura']])
+--         vim.cmd([[colorscheme edge]])
+--     end
+-- }
 return {
-    "sainnhe/edge",
-    priority = 1000, -- load this before anything else
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
     config = function()
-        vim.cmd([[let g:edge_style='aura']])
-        vim.cmd([[colorscheme edge]])
-    end
+        require('catppuccin').setup({
+            transparent_background = true, -- disables setting the background color.
+        })
+
+        -- vim.cmd([[let g:edge_style='aura']])
+        vim.cmd([[colorscheme catppuccin-macchiato]])
+    end,
 }
