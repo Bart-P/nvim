23836057d7397926f4384895dcce7a5192787bdc
Bart P. <bartek.paraf@gmail.com>
2025-04-17 09:43:19 +0200

    added todo-comments and ts-comments plugins

--- lua/bp/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Copy selected to clipboard
-vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy to clipboard" })
-vim.keymap.set("n", "<leader>yy", '"+yy', { desc = "Copy line to clipboard" })
-
--- Paste from clipboard
-vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
